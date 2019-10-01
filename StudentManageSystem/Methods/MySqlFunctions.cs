a1a98f1deff25d7112c433798858a4ab0692ef5a
jmp0 <302864369@qq.com>
2019-10-01 15:44:15 +0800

    最初的项目

---
+++ StudentManageSystem/Methods/MySqlFunctions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace StudentManageSystem.Methods
+{
+    public class MySqlFunctions
+    {
+        private SqlConnection sqlConnection = null;
+        public bool isconnected = false;
+        public MySqlFunctions(HttpResponse response) {
+            //string str = System.Configuration.ConfigurationManager.ConnectionStrings["MyDB"].ConnectionString;
+            string connString = "data source=49.235.184.242,3306;initial catalog=mysql;user id=root;pwd=root";
+            try {
+                sqlConnection = new SqlConnection(connString);
+                sqlConnection.Open();
+            }
+            catch (Exception e) {
+                response.Write("<script>alert('数据库链接失败')</script>");
+                return;
+            }
+            isconnected = true;
+            response.Write("<script>alert('数据库链接成功')</script>");
+        }
+    }
+}
---
+++ StudentManageSystem/Pages/test.aspx.cs
@@ -0,0 +1,24 @@
+﻿using StudentManageSystem.Methods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace StudentManageSystem.Pages
+{
+    public partial class test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MySqlFunctions mySql = new MySqlFunctions(Response);
+
+        }
+    }
+}
